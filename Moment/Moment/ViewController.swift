32602638ca27d93bfc3f567b09d9fbcd307fd926
Sam Lau <samlaudev@gmail.com>
2021-04-15 20:14:28 +0800

    Create moment project

---
+++ Moment/Moment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moment
+//
+//  Created by Sam Lau on 2021/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
