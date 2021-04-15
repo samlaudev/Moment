c5c098ee9af47ba78a97d25ffcb14a17b4b60090
Sam Lau <samlaudev@gmail.com>
2021-04-15 15:38:54 +0800

    fix setup.sh script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install ruby using rbenv
-ruby_version = `cat .ruby-version`
+ruby_version=`cat .ruby-version`
 if [[ ! -d "$HOME/.rbenv/versions/$ruby_version" ]]; then
     rbenv install $ruby_version
 fi
