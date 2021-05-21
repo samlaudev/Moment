d930ad82195f5edde5bae8a12fff674ff504e6e4
Sam Lau <samlaudev@gmail.com>
2021-05-21 11:53:07 +0800

    Add firebase support

--- Gemfile
+++ Gemfile
@@ -6,3 +6,6 @@ gem "cocoapods", "1.10.0"
 gem "fastlane", "2.180.1"
 
 
+
+plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
+eval_gemfile(plugins_path) if File.exist?(plugins_path)
--- Moment/Moment/AppDelegate.swift
+++ Moment/Moment/AppDelegate.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        onLaunch()
         return true
     }
 
@@ -26,5 +28,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+private extension AppDelegate {
+    func onLaunch() {
+        // Load a named file.
+        let filePath = Bundle.main.path(forResource: try? Configuration.value(for: "FIREBASE_CONFIG_FILENAME"), ofType: "plist")!
+        guard let fileopts = FirebaseOptions(contentsOfFile: filePath)
+          else { assert(false, "Couldn't load config file") }
+        FirebaseApp.configure(options: fileopts)
     }
 }
