b1dcb3fa17f481e99189d59e4dd3381d88f0ff89
Sam Lau <samlaudev@gmail.com>
2021-06-03 16:59:54 +0800

    Add standard Spacing

---
+++ Frameworks/DesignKit/DesignKit/Classes/Spacing/Spacing.swift
@@ -0,0 +1,17 @@
+//
+//  Spacing.swift
+//  DesignKit
+//
+//  Created by Sam Lau on 2021/6/3.
+//
+
+public struct Spacing {
+    public static let twoExtraSmall: CGFloat = 4
+    public static let extraSmall: CGFloat = 8
+    public static let small: CGFloat = 12
+    public static let medium: CGFloat = 18
+    public static let large: CGFloat = 24
+    public static let extraLarge: CGFloat = 32
+    public static let twoExtraLarge: CGFloat = 40
+    public static let threeExtraLarge: CGFloat = 48
+}
